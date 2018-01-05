d66c40261b81c5cc322624db9036c6df06b6b240
Findley, Randy <rgfindley@gmail.com>
2018-01-05 11:19:49 -0500

    trigger build.

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/*', (req, res) => {
-    res.send('Services Stack with CI/CD! v3  '+req.path+'\n');
+    res.send('Services Stack with CI/CD! v4  '+req.path+'\n');
 });
 
 app.listen(PORT, HOST);
